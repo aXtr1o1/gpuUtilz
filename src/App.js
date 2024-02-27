8a3612fa8bc2e52cb57ab2fa667cd25cb0123970
Sid <siddiqmoideen07@gmail.com>
2024-02-27 11:26:04 +0530

    Updated LandingPage Yo!

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
-import React, { useEffect, useState } from 'react';
+import './App.css';
 import logo from './buttonStuff.svg';
 
-
-function App(){
+function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <a href="/"> 
-      {/* function name to be excecuted onclick of the button */}
+      <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">
         <img src={logo} className="App-logo" alt=" " />
       </a>
       </header>
@@ -15,40 +13,4 @@ function App(){
   );
 }
 
-const GPUInfoComponent = () => {
-  const [gpuInfo, setGPUInfo] = useState(null);
-
-  useEffect(() => {
-    // Check for WebGL support
-    const canvas = document.createElement('canvas');
-    const gl = canvas.getContext('webgl') || canvas.getContext('experimental-webgl');
-    if (!gl) {
-      console.error('WebGL is not supported in this browser.');
-      return;
-    }
-
-    // Get GPU renderer and vendor
-    const renderer = gl.getParameter(gl.RENDERER);
-    const vendor = gl.getParameter(gl.VENDOR);
-
-    setGPUInfo({ renderer, vendor });
-
-  }, []);
-
-  return (
-    <div>
-      {gpuInfo ? (
-        <div>
-          <h2>GPU Details</h2>
-          <p>Renderer: {gpuInfo.renderer}</p>
-          <p>Vendor: {gpuInfo.vendor}</p>
-        </div>
-      ) : (
-        <p>Loading GPU information...</p>
-      )}
-    </div>
-  );
-};
-
-export default GPUInfoComponent;
-// export default App;
+export default App;
