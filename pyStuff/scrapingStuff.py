71fd17eb1eaa3d82bf76a839aaa6bd7a9b4a6f33
rauf8888 <mrauf2907@gmail.com>
2024-03-05 23:14:15 +0530

    File Updated: Updated to Scrap RAW Contents from website

--- pyStuff/scrapingStuff.py
+++ pyStuff/scrapingStuff.py
@@ -1,21 +1,21 @@
-import pprint
+from pprint import pprint
 from colorama import Fore, Style
 from tqdm import tqdm
 from tabulate import tabulate
 from bs4 import BeautifulSoup
 import requests
+import csv
 
 webpage = requests.get('https://en.wikipedia.org/wiki/CUDA')
 
 cuda_table = BeautifulSoup(webpage.text,'html.parser')
 table_contents = cuda_table.find_all("table",class_="wikitable")
-if len(table_contents) > 2:
-    print("/////\n\n")
-    pprint.pprint(table_contents[1])
-    print("\n\n/////")
 
+compute_compatibility = table_contents[1]
+Nvidia_GPU_BoardProducts = table_contents[2]
 
-
-
-
-
+pprint(compute_compatibility)
+print("\n\n\n\n\n")
+pprint(Nvidia_GPU_BoardProducts)
+print(type(cuda_table))
+print(type(table_contents))
