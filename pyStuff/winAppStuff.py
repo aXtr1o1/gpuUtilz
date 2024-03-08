b93f55a7104553e90d88be5cb11f2da490ccce33
Praga3004 <pragadeswaran.m.s@gmail.com>
2024-03-08 20:19:33 +0530

    Initialized kivy-Based Windows App

--- pyStuff/winAppStuff.py
+++ pyStuff/winAppStuff.py
@@ -1,24 +1,39 @@
-from colorama import Fore, Style
-from tabulate import tabulate
-from tqdm import tqdm
+import kivy 
 import GPUtil
+kivy.require("2.3.0")
+from tqdm import tqdm
+from kivy.app import App 
+from kivy.uix.label import Label
+import tabulate
+from colorama import Fore, Style
 
-GPUs = GPUtil.getGPUs()
-GPUlist = []
-for i in tqdm(GPUs, desc = "Flowin' throuogh the GPUs Yo!", colour = 'red'):
-    gpuId = i.id
-    gpuName = i.name
-    gpuLoad = f"{i.load*100}%"
-    gpuFreeMemory = f"{i.memoryFree}MB"
-    gpuUsedMemory = f"{i.memoryUsed}MB"
-    gpuTotalMemory = f"{i.memoryTotal}MB"
-    gpuTemperature = f"{i.temperature} °C"
-    gpuUUid = i.uuid
-    GPUlist.append((
-        gpuId, gpuName, gpuLoad, gpuFreeMemory, gpuUsedMemory,
-        gpuTotalMemory, gpuTemperature, gpuUUid
-    ))
+class Labapp(App):
+    def build(self):
+        Gpus=GPUtil.getGPUs()
+        if Gpus:
+            Gpu_list=[]
+            for i in tqdm(Gpus, desc="Flowin' through the GPUs Yo!", colour='red'):
+                gpuId = i.id
+                gpuName = i.name
+                gpuLoad = f"{i.load*100}%"
+                gpuFreeMemory = f"{i.memoryFree}MB"
+                gpuUsedMemory = f"{i.memoryUsed}MB"
+                gpuTotalMemory = f"{i.memoryTotal}MB"
+                gpuTemperature = f"{i.temperature} °C"
+                gpuUUid = i.uuid
+                Gpu_list.append((
+                    gpuId, gpuName, gpuLoad, gpuFreeMemory, gpuUsedMemory,
+                    gpuTotalMemory, gpuTemperature, gpuUUid
+                ))
+            output = f"{Fore.BLUE+Style.BRIGHT}\n\nGPUstuff Yo !{Fore.RESET}\n"
+            output += tabulate(Gpu_list, headers=("id", "name", "load", "free memory", "used memory", "total memory",
+                                              "temperature", "uuid"))
+            
+            lab=Label(text=output,color="red")
+            return lab
+        else:
+            lab=Label(text="No Details About GPU  404 Error",color="red")
+            return lab
 
-print(Fore.BLUE+Style.BRIGHT+"\n\nGPUstuff Yo !"+Fore.RESET)
-print(tabulate(GPUlist, headers=("id", "name", "load", "free memory", "used memory", "total memory",
-                                   "temperature", "uuid")))
+label=Labapp()
+label.run()
--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">
+      <a href="https://drive.google.com/file/d/1RxvBxeJ7ZH3l3anX52rLi2rbl7tWprmR/view?usp=sharing">
         <img src={logo} className="App-logo" alt=" " />
       </a>
       </header>
