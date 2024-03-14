1e9e1cbb73b4ae74cdf2c3f678e8a965c3398c91
rauf8888 <mrauf2907@gmail.com>
2024-03-14 23:03:14 +0530

    File Modified: Collected Data Prettyfied

--- pyStuff/textFileManipulation.py
+++ pyStuff/textFileManipulation.py
@@ -1,12 +1,21 @@
+from pprint import pprint
 with open("Cuda_Version.txt",'r') as cudafile:
     contents = cudafile.readlines()
-    # print(contents,'\n')
 
 # print('\n\n\n\n\n')
 
 with open("CUDNN_Version.txt",'r') as cudnnFile:
     cdncontents = cudnnFile.readlines()
     # print(cdncontents)
-print(type(contents))
-print(list(cudnnFile))
 
+for i in range(len(contents)):
+    print(contents[i],'\n')
+print('\n')
+pprint(cdncontents)
+
+print('\n\n')
+print(type(contents))
+print(type(cdncontents))
+print('\n')
+print(len(contents))
+print(len(cdncontents))
