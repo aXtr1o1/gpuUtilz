3f23503a69f7c2e2bad45a72251ee57da53e59c9
rauf8888 <mrauf2907@gmail.com>
2024-03-18 14:10:46 +0530

    File Modified: Scraps out Compute Compatibility table and saves in CSV

--- pyStuff/scrapingStuff.py
+++ pyStuff/scrapingStuff.py
@@ -18,7 +18,6 @@ cudnn_archive_list =[]
 for item in a_tag_list:
     cudnn_text = str(item)+"\t"+'\n'
     cudnn_archive_list.append(cudnn_text)
-# pprint(cudnn_archive_list)
 cudnn_contents = [x.split("for") for x in cudnn_archive_list]
 cudnn_contents = [[sublist.replace('\n','').replace('\t','') for sublist in megalist] for megalist in cudnn_contents]
 
@@ -34,17 +33,30 @@ if not os.path.exists("cuDNN_CUDA_Version_Compatibility.csv"):
 wiki_webpage = requests.get('https://en.wikipedia.org/wiki/CUDA')
 cuda_table = BeautifulSoup(wiki_webpage.text,'html.parser')
 table_contents = cuda_table.find_all("table",class_="wikitable")
-
 compute_compatibility = table_contents[1]
-Nvidia_GPU_BoardProducts = table_contents[2]
 
-
-pprint(compute_compatibility.find('caption').text)
-pprint('\n\n')
+#Scraping RAW data from Wikipedia Table 
 table_rows = compute_compatibility.find_all('tr')
-header_row = [x.text.strip() for x in table_rows[0]]
-print(header_row)
+i= 0
+header_row = [table_rows[i + index].text.strip() for index, _ in enumerate(table_rows)]
 
+compatibility_list = []
+for i in range(len(header_row)):
+    compatibility_list.append(header_row[i].strip().split('\n'))
+compatibility_list = [[item.split('[')[0] for item in sublist] for sublist in compatibility_list]
+for i in compatibility_list:
+    elements_to_add = 13 - len(i)
+    i.extend(['']*elements_to_add)
+
+#Adding the Scraped out compatibility data in a CSV file
+if not os.path.exists("CUDA_Compute_Compatibility-CUDA SDK support vs Microarchitechture.csv"):
+    with open("CUDA_Compute_Compatibility-CUDA SDK support vs Microarchitechture.csv",'w') as compat_csv:
+        compat_data = csv.writer(compat_csv)
+        compat_data.writerow([compute_compatibility.find('caption').text])
+        compat_data.writerows(compatibility_list)
+
+# Nvidia GPU BoardProducts From Wikipedia
+Nvidia_GPU_BoardProducts = table_contents[2]
 # text=""
 # table=Nvidia_GPU_BoardProducts.find_all('tr')
 # table=table[1:]
