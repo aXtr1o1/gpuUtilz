07f2453302bfb662f9747c917e30899689a2e4c9
rauf8888 <mrauf2907@gmail.com>
2024-03-19 10:10:46 +0530

    File Modified: Scrapped Out Nvidia Board Compatibility Data

--- pyStuff/scrapingStuff.py
+++ pyStuff/scrapingStuff.py
@@ -7,77 +7,89 @@ import requests
 import csv
 import os
 
-cudnn_archive = requests.get("https://developer.nvidia.com/rdp/cudnn-archive")
+# cudnn_archive = requests.get("https://developer.nvidia.com/rdp/cudnn-archive")
 
-cudnn_list = BeautifulSoup(cudnn_archive.text,'html.parser')
-list_contents = cudnn_list.find("div",class_="panel-group")
-a_tag_list = [a.get_text(strip=True) for a in list_contents.find_all('a') if a.get_text(strip=True).startswith("Download") ]
+# cudnn_list = BeautifulSoup(cudnn_archive.text,'html.parser')
+# list_contents = cudnn_list.find("div",class_="panel-group")
+# a_tag_list = [a.get_text(strip=True) for a in list_contents.find_all('a') if a.get_text(strip=True).startswith("Download") ]
 
-#Prettyfying the raw scraped ugly data#
-cudnn_archive_list =[]
-for item in a_tag_list:
-    cudnn_text = str(item)+"\t"+'\n'
-    cudnn_archive_list.append(cudnn_text)
-cudnn_contents = [x.split("for") for x in cudnn_archive_list]
-cudnn_contents = [[sublist.replace('\n','').replace('\t','') for sublist in megalist] for megalist in cudnn_contents]
+# #Prettyfying the raw scraped ugly data#
+# cudnn_archive_list =[]
+# for item in a_tag_list:
+#     cudnn_text = str(item)+"\t"+'\n'
+#     cudnn_archive_list.append(cudnn_text)
+# cudnn_contents = [x.split("for") for x in cudnn_archive_list]
+# cudnn_contents = [[sublist.replace('\n','').replace('\t','') for sublist in megalist] for megalist in cudnn_contents]
 
-#Adding the Waterwashed data in a CSV file#
-if not os.path.exists("cuDNN_CUDA_Version_Compatibility.csv"):
-    with open("cuDNN_CUDA_Version_Compatibility.csv",'w') as cudnn_csv:
-        header = ['cuDNN Version','CUDA Version']
-        data = csv.writer(cudnn_csv)
-        data.writerow(header)
-        data.writerows(cudnn_contents)
+# #Adding the Waterwashed data in a CSV file#
+# if not os.path.exists("cuDNN_CUDA_Version_Compatibility.csv"):
+#     with open("cuDNN_CUDA_Version_Compatibility.csv",'w') as cudnn_csv:
+#         header = ['cuDNN Version','CUDA Version']
+#         data = csv.writer(cudnn_csv)
+#         data.writerow(header)
+#         data.writerows(cudnn_contents)
 
 # CUDA Version Scraping From WikiPedia
 wiki_webpage = requests.get('https://en.wikipedia.org/wiki/CUDA')
 cuda_table = BeautifulSoup(wiki_webpage.text,'html.parser')
 table_contents = cuda_table.find_all("table",class_="wikitable")
-compute_compatibility = table_contents[1]
+# compute_compatibility = table_contents[1]
 
-#Scraping RAW data from Wikipedia Table 
-table_rows = compute_compatibility.find_all('tr')
-i= 0
-header_row = [table_rows[i + index].text.strip() for index, _ in enumerate(table_rows)]
+# #Scraping RAW data from Wikipedia Table 
+# table_rows = compute_compatibility.find_all('tr')
+# i=0
+# header_row = [table_rows[i + index].text.strip() for index, _ in enumerate(table_rows)]
 
-compatibility_list = []
-for i in range(len(header_row)):
-    compatibility_list.append(header_row[i].strip().split('\n'))
-compatibility_list = [[item.split('[')[0] for item in sublist] for sublist in compatibility_list]
-for i in compatibility_list:
-    elements_to_add = 13 - len(i)
-    i.extend(['']*elements_to_add)
+# #Prettifying the Compute Compatibility Table
+# compatibility_list = []
+# for i in range(len(header_row)):
+#     compatibility_list.append(header_row[i].strip().split('\n'))
+# compatibility_list = [[item.split('[')[0] for item in sublist] for sublist in compatibility_list]
+# for i in compatibility_list:
+#     elements_to_add = 13 - len(i)
+#     i.extend(['']*elements_to_add)
 
-#Adding the Scraped out compatibility data in a CSV file
-if not os.path.exists("CUDA_Compute_Compatibility-CUDA SDK support vs Microarchitechture.csv"):
-    with open("CUDA_Compute_Compatibility-CUDA SDK support vs Microarchitechture.csv",'w') as compat_csv:
-        compat_data = csv.writer(compat_csv)
-        compat_data.writerow([compute_compatibility.find('caption').text])
-        compat_data.writerows(compatibility_list)
+# #Adding the Scraped out compatibility data in a CSV file
+# if not os.path.exists("CUDA_Compute_Compatibility-CUDA SDK support vs Microarchitechture.csv"):
+#     with open("CUDA_Compute_Compatibility-CUDA SDK support vs Microarchitechture.csv",'w') as compat_csv:
+#         compat_data = csv.writer(compat_csv)
+#         compat_data.writerow([compute_compatibility.find('caption').text])
+#         compat_data.writerows(compatibility_list)
 
 # Nvidia GPU BoardProducts From Wikipedia
 Nvidia_GPU_BoardProducts = table_contents[2]
-# text=""
-# table=Nvidia_GPU_BoardProducts.find_all('tr')
-# table=table[1:]
-# key= [x.find('td').text.strip() for x in table if x.find('td')]
-# val=[]
-# for x in table:
-#     if x.find('td'):
-#         row_vals = []
-#         for td in x.find_all('td'):
-#             row_vals.append(td.text.strip())
-#         val.append(row_vals[1:])
-# if not os.path.exists("CUDA_version.txt"):
-#     with open('CUDA_version.txt','w') as f:
-#         f.write("\t\t\t\tCuda Version Bruh\t\n")
-#         for i in range(len(key)):
-#             value=[x for x in val[i] if x !='']
-#             value = [x.replace('\xa0K1','').replace('\xa0TX1','').replace('\xa0','') if '\xa0K1' in x or '\xa0TX1' in x or '\xa0' in x  else x   for x in value]
-#             text=str(key[i])+"\t:\t"+str(value)+"\n"
-#             f.write(text)
-#     print("Success")
+text=""
+table=Nvidia_GPU_BoardProducts.find_all('tr')
+table=table[1:]
+key= [x.find('td').text.strip() for x in table if x.find('td')]
+val=[]
+for x in table:
+    if x.find('td'):
+        row_vals = []
+        for td in x.find_all('td'):
+            row_vals.append(td.text.strip())
+        val.append(row_vals[1:])
 
+# with open('CUDA_version.txt','w') as f:
+#     f.write("\t\t\t\tCuda Version Bruh\t\n")
+#     for i in range(len(key)):
+#         value=[x for x in val[i] if x !='']
+#         value = [x.replace('\xa0K1','').replace('\xa0TX1','').replace('\xa0','') if '\xa0K1' in x or '\xa0TX1' in x or '\xa0' in x  else x   for x in value]
+#         text=str(key[i])+"\t:\t"+str(value)+"\n"
+#         pprint(text)
+#         f.write(text)
+# print("Success")
+Board_compt = []
+for i in range(len(key)):
+        value=[x for x in val[i] if x !='']
+        value = [x.replace('\xa0K1','').replace('\xa0TX1','').replace('\xa0','') if '\xa0K1' in x or '\xa0TX1' in x or '\xa0' in x  else x   for x in value]
+        text=str(key[i])+"\t:\t"+str(value)+"\n"
+        Board_compt.append(text)
+pprint(Board_compt)
+# with open('Nvidia_GPU_Board_Compatibility.csv','w') as bcomp:
+#     bcomp_data = csv.writer(bcomp)
+#     bcomp_data.writerow(['Compute Capability (version)','Nvidia GPU Board'])
+#     bcomp_data.writerows
 
 # print(val)
 # pprint(compute_compatibility)
